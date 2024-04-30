adf4706894e5ada4113b05d6b5de7b12bdda5c47
doremii4389 <ridhoahmadfauzan38@gmail.com>
2024-04-30 16:50:52 +0700

    nambah paragraf

---
+++ dfgh.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos 
+        voluptas, consequatur explicabo iusto nihil eaque rem amet aut, maiores facilis praesentium saepe similique ipsam reiciendis, velit optio blanditiis modi.</p>
+</body>
+
+</html>
